d2db6e8b02e00f635c266798ded58ba7961849a2
Nic_Logan <nicholaslogan255@gmail.com>
2018-07-23 22:43:48 -0500

    Scoring and showing match complete

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,19 +10,52 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         console.log("Posting to api/friends");
-       console.log( "req.body: ",req.body);
+        
 
-        friend.push(req.body);
+        // grade scores of other users and find best match
+        console.log( "================================================================");
 
-        // TODO: grade scores of other users and find best match
+        // the total scores of each of the other users
+        var friendRatings = [];
 
-        // go through every friend
-        for(var i in friends){
+        // go through every friend, find score
+        for (var i = 0; i < friend.length; i++) {
+            console.log("Rating: "+friend[i].name);
+            var curFriendScore = 0;
 
-            for(var j in friends.score){
+            for (var j in friend[i].scores) {
+                //console.log(friend[i].scores[j]);
 
+                //console.log(`${j}: ${req.body.scores[j]} - ${friend[i].scores[j]}  ${5 - Math.abs(friend[i].scores[j] - req.body.scores[j] )}`);
+
+                // compare answers; add 5 if matching answers,  4 if different by 1, etc..
+                // it gives you more points the closer your two answers are
+                curFriendScore += 5 - Math.abs(friend[i].scores[j] - req.body.scores[j]);
+            }
+
+            // add the final score to the list of potential friends
+            friendRatings.push(curFriendScore);
+        }
+        console.log( "================================================================");
+
+        console.log("friend scores: " + friendRatings);
+
+        // find best friend
+        var bestFriendInd = 0;
+        for(var k in friendRatings){
+            if(friendRatings[k] > friendRatings[bestFriendInd]){
+                bestFriendInd = k;
             }
         }
+
+        // log best friend
+        console.log("Best Friend: ",friend[bestFriendInd] );
+
+        // add current user to list of users (done after so site doesn't try to match with self)
+        friend.push(req.body);
+
+        // return the best friend 
+        res.json(friend[bestFriendInd]);
 
 
     });
