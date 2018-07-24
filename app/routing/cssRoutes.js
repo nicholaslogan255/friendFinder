020660ac3544d74e49d6757791a89103f4671b93
Nic_Logan <nicholaslogan255@gmail.com>
2018-07-23 23:14:19 -0500

    Updated CSS

--- app/routing/cssRoutes.js
+++ app/routing/cssRoutes.js
@@ -9,5 +9,10 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/css/main.css"));
     });
 
+    app.get("/css/background", function (req, res) {
+        console.log("Getting people.jpg");
+        res.sendFile(path.join(__dirname, "../data/people.jpg"));
+    });
+
     
 }
